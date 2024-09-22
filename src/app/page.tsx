521b71d10349300b11c04106357680a1c9600c42
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-09-22 14:24:16 -0500

    🆕 Pagina principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
+import { UserInfo } from "@/components/user-info"
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      Hola Mundo
+    <main className="max-w-screen-lg mx-auto px-4 lg:px-0">
+      <h1 className="text-4xl font-bold text-center">
+        <a 
+        className="hover:text-blue-500 transition-colors" 
+        href="https://nextjs.org/docs" 
+        target="_blank"
+        >
+          Auth.js
+        </a>
+      </h1>
+      <p className="text-center text-zinc-200 mt-3">
+        Ejemplo de autenticación con la libreria <a 
+        className="hover:text-blue-500 transition-colors" 
+        href="https://nextjs.org/docs" 
+        target="_blank"
+        >
+          Auth.js
+        </a>. 
+      </p>
+      <p className="text-center text-zinc-200 mt-1 mb-5">
+        <strong>Providers utilizados:</strong> Github
+      </p>
+
+      <UserInfo />
     </main>
   )
 }
